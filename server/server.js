241967ef099c26d056a3f46332ecf5b90e859bc9
Jacob Hildebrandt <jake@hildy.ca>
2021-04-26 16:46:10 -0700

    Created Listing Schema

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 dotenv.config({ path: ".env" });
 
@@ -18,6 +22,31 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful!"));
+
+const listingSchema = new mongoose.Schema({
+  creatorId: { type: String, required: [true, "A listing must a creatorId"] },
+  posted: { type: Boolean, default: false },
+  sold: { type: Boolean, default: false },
+  title: { type: String, required: [true, "A listing must have a title"] },
+  listPrice: {
+    type: Number,
+    required: [true, "A listing must have a list price"],
+  },
+  listCurrency: { type: String, default: "ETH" },
+  category: {
+    type: String,
+    required: [true, "A listing must have a category"],
+  },
+  location: { lat: String, long: String },
+  description: {
+    type: String,
+    required: [true, "A listing must have a description"],
+  },
+  images: [{ id: String, url: String }],
+  followers: [String],
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
 
 const PORT = process.env.PORT || 8080;
 
