7d15d892a207c629cee91a7775e5f274dbb55181
Jacob Hildebrandt <jake@hildy.ca>
2021-04-26 16:04:08 -0700

    Setup base server and connected to remote MongoDB using mongoose

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 
-app.listen();
+dotenv.config({ path: ".env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`App Listening On port ${PORT}`));
