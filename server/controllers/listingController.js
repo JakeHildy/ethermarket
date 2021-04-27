63620b5f4cead94c5cb4ceca0b4227d1e5820c65
Jacob Hildebrandt <jake@hildy.ca>
2021-04-27 09:12:13 -0700

    Refactored Api Features into separate file

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -1,5 +1,6 @@
 const Listing = require("./../models/listingModel");
 const express = require("express");
+const APIFeatures = require("./../utils/apiFeatures");
 const { query } = require("express");
 
 exports.aliasRecentSix = (req, _res, next) => {
@@ -8,57 +9,6 @@ exports.aliasRecentSix = (req, _res, next) => {
   req.query.fields = "createdAt,title,price,images";
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    // Filtering
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(",").join(" ");
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select("-__v");
-    }
-    return this;
-  }
-
-  paginate() {
-    // page=2&limit=10 ==> 1-10 -> page 1, 11-20 -> page 2, etc...
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 20;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 exports.getAllListings = async (req, res) => {
   try {
