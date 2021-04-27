1da0a20e8d721276a7e31978c2112b4750f32073
Jacob Hildebrandt <jake@hildy.ca>
2021-04-26 21:53:30 -0700

    Added field limiting to Get all Listings

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -29,6 +29,14 @@ exports.getAllListings = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     // EXECUTE QUERY
     const listings = await query;
 
